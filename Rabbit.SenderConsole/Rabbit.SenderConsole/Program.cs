ad66ba006543917ce986c0291f513dfa3619cd2a
Danieli Cywyak <danielicy@hotmail.com>
2016-08-03 10:24:36 +0300

    created senderConsole

---
+++ Rabbit.SenderConsole/Rabbit.SenderConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using Sender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbit.SenderConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendHelper sender = new SendHelper();
+            do
+            {
+                Console.WriteLine("Enter something:\n");
+
+                sender.SendMessage(Console.ReadLine());
+            }
+            while (Console.ReadLine() != "0");
+        }
+    }
+}
---
+++ Rabbit.SenderConsole/Sender/SendHelper.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    public class SendHelper
+    {
+        public void SendMessage(string msg)
+        {
+
+            var factory = new ConnectionFactory() { HostName = ConfigurationManager.AppSettings["RabbitServer"] };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+
+                var properties = channel.CreateBasicProperties();
+
+                properties.Persistent = true;
+
+
+                string message = msg;
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "task_queue",
+                                     basicProperties: properties,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
